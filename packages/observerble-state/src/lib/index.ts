c6f1a2014f07d470355d71e3646b5c51ef9bd354
cuitianliang <cuitianliang@hnnewsoft.com>
2022-07-27 18:41:18 +0800

    feat: add exprts

--- packages/observerble-state/src/lib/index.ts
+++ packages/observerble-state/src/lib/index.ts
@@ -1,6 +1,18 @@
 /**
  * @file observerble-state libaray entry js
  */
+// import createGlobalState, {createSimpleGlobalState} from './createGlobalState'
 import welcome from './welcome'
+
+export {
+  default as createGlobalState,
+  createSimpleGlobalState,
+} from './createGlobalState'
+export { default as Observerble, SubscribeObserver } from './Observerble'
+export {
+  useDepsFn,
+  default as useObserverble,
+  useSimpleObserverble,
+} from './useObserverble'
 
 export { welcome }
